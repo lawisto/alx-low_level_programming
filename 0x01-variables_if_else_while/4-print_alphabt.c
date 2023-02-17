bdd4a26808381ee0fddddf0b704bd35935e7a383
lawisto <achokorlawi@gmail.com>
2023-02-17 13:06:59 -0800

    4-print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - print lowercase alphabet except q and e
+*
+*Return: 0 (Success)
+*/
+int main(void)
+{
+	char lower;
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		if(lower !='q' && lower !='e')
+		{
+			putchar(lower);
+		}
+		else
+		{
+		}
+	}
+putchar('\n');
+return (0);
+}
