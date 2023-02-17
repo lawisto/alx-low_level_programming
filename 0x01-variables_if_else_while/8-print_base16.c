71306ba786d61b975359b0779e1f3f63ebb4a917
lawisto <achokorlawi@gmail.com>
2023-02-17 13:33:43 -0800

    8-base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - print hexadecimal numbers
+*
+*Return: 0 (Success)
+*/
+int main(void)
+{
+	char num;
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	for (num = 97; num <= 102; num++)
+	{
+	putchar(num);
+	}
+putchar('\n');
+return (0);
+}
