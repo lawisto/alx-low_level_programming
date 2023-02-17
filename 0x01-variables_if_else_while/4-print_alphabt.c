14a13beae6bcd51d27fcfb6ea2ded6737fe7ad68
lawisto <achokorlawi@gmail.com>
2023-02-17 13:24:33 -0800

    6-print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,6 +13,7 @@ int main(void)
 		{
 			putchar(lower);
 		}
+
 		else
 		{
 		}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - outputs numbers from 1 to 9
+*
+*Return: 0 (Success)
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+putchar('\n');
+return (0);
+}
