eaf1f640f223fb2dd7cbe57eee18ee71bcb4f454
lawisto <achokorlawi@gmail.com>
2023-02-17 13:28:01 -0800

    7-print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - print alphabets in reverse
+*
+*Return: 0 (Success)
+*/
+int main(void)
+{
+	char lowerRev;
+	for (lowerRev = 'z'; lowerRev >= 'a'; lowerRev--)
+	{
+		putchar(lowerRev);
+	}
+putchar('\n');
+return (0);
+}
