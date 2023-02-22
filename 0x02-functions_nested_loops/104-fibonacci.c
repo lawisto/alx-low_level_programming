428b30ea560ee02fa355877e1ced76145034d9c3
lawisto <achokorlawi@gmail.com>
2023-02-22 00:24:39 -0800

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,31 +1,41 @@
-#include "main.h"
 #include <stdio.h>
+
 /**
-*main - prints first 98 Fibonacci numbers
-*
-*Return: 0 (Success)
-*/
+ * main - print fibonacci
+ *
+ * Return: always 0
+ */
 int main(void)
 {
-	long int prev, curr, next;
-	int counter;
+	unsigned long int a, a1, a2, b, b1, b2, c, c1, c2, d, e;
 
-	prev = 1;
-	curr = 2;
-	next = prev + curr;
+	a = 1;
+	b = 2;
+	c = a + b;
 
-	printf("%ld, %ld", prev, curr);
-
-	for (counter = 2; counter < 98; counter++)
+	printf("%lu, ", a);
+	printf("%lu, ", b);
+	for (d = 3; d < 89; d++)
 	{
-		if (next > 7540113804746346429)
-		{		
-			next = prev + curr;
-			printf(", %ld", next);
-			prev = curr;
-			curr = next;
-		}
+		printf("%lu, ", c);
+		a = b;
+		b = c;
+		c = a + b;
 	}
-	printf("\n");
+	b1 = b / 1000000000;
+	b2 = b % 1000000000;
+	c1 = c / 1000000000;
+	c2 = c % 1000000000;
+	for (e = 89; e < 98; e++)
+	{
+		printf("%lu%lu, ", c1, c2);
+		a1 = b1;
+		a2 = b2;
+		b1 = c1;
+		b2 = c2;
+		c1 = a1 + b1 + ((a2 + b2) / 1000000000);
+		c2 = (a2 + b2) % 1000000000;
+	}
+printf("%lu%lu\n", c1, c2);
 	return (0);
 }
