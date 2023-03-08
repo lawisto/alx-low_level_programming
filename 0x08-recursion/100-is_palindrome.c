e7715c16797e48f52a20b7972f86ca0792ad516b
lawisto <achokorlawi@gmail.com>
2023-03-08 07:44:01 -0800

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,51 +1,55 @@
 #include "main.h"
-
 /**
- * last_index - returns the last index of a string (counts the null char)
- * @s: pointer the string
- * Return: int
- */
-
-int last_index(char *s)
+*palindromefn - Determines if string is palindrome
+*
+*@i:index of first character in string
+*@s:pointer to string
+*@strlen: string length
+*@j:index of last charater in string
+*
+*Return: 1 if palindrome else 0
+*/
+int palindromefn(int i, char *s, int strlen, int j)
 {
-int n = 0;
-
-if (*s > '\0')
-	n += last_index(s + 1) + 1;
-
-return (n);
+	if (i >= strlen)
+	{
+		return (1);
+	}
+	else if (s[i] != s[j])
+	{
+		return (0);
+	}
+	else
+	{
+		return (palindromefn(++i, s, strlen, --j));
+	}
 }
-
 /**
- * is_palindrome - check if a string is a palindrome
- * @s: string to check
- * Return: 0 or 1
- */
-
+*_strlen_recursion - calculates string length
+*
+*@s: pointer to first character in string
+*
+*Return: stirng length
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(++s));
+	}
+}
+/**
+*is_palindrome - determines if string is palindrome
+*
+*@s: pointer to string
+*
+*Return: 1 if palindrome else 0
+*/
 int is_palindrome(char *s)
 {
-int end = last_index(s);
-
-return (check(s, 0, end - 1, end % 2));
-}
-
-/**
- * check - checker for the palindrome
- * @s: string
- * @start: int moves from right to left
- * @end: int moves from left to right
- * @pair: int
- * Return: 0 or 1
- */
-
-
-int check(char *s, int start, int end, int pair)
-{
-
-if ((start == end && pair != 0) || (start == end + 1 && pair == 0))
-	return (1);
-else if (s[start] != s[end])
-	return (0);
-else
-	return (check(s, start + 1, end - 1, pair));
+	return (palindromefn(0, s, _strlen_recursion(s), (_strlen_recursion(s) - 1)));
 }
