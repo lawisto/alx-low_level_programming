8c045deefd5237a79bcf940ef88d4559a5bd8e4f
lawisto <achokorlawi@gmail.com>
2023-03-30 04:53:36 -0700

    firs

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+* before_main - function executed bfore main
+*/
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
